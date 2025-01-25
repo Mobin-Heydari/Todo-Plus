39ca5c3b1eeccfa5282170ec21d2a0e9d8a3fc34
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-01-25 23:42:04 +0330

    OneTimePasswordSerializer class added with the create method to authentication app.

--- Backend/Server/Authentication/serializers.py
+++ Backend/Server/Authentication/serializers.py
@@ -6,6 +6,12 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from Users.models import User
 
+from .models import OneTimePassword
+
+from random import randint
+import uuid
+
+
 
 class TokenObtainSerializer(TokenObtainPairSerializer):
     # Custom Token Obtain Serializer that inherits from TokenObtainPairSerializer
@@ -248,3 +254,33 @@ class UserRegisterSerializer(serializers.Serializer):
                 'access': str(refresh.access_token),
             }
         }
+    
+# Define the OneTimePasswordSerializer class
+class OneTimePasswordSerializer(serializers.ModelSerializer):
+    # Define the Meta class to specify the model and fields
+    class Meta:
+        # Specify the model for the serializer
+        model = OneTimePassword
+        # Specify all fields from the model
+        fields = "__all__"
+
+    # Define the create method to create a new OneTimePassword instance
+    def create(self, validated_data):
+        # Generate a random 6-digit code for the OTP
+        code = randint(100000, 999999)
+        
+        # Create a new OneTimePassword instance with the provided user and generated code
+        otp = OneTimePassword.objects.create(
+            # Set the user for the OTP
+            user=validated_data['user'],
+            # Set the code for the OTP
+            code=code,
+            # Generate a UUID for the token
+            token=uuid.uuid4()
+        )
+
+        # Save the new OneTimePassword instance to the database
+        otp.save()
+
+        # Return the token for the newly created OTP
+        return {'token': str(otp.token)}
