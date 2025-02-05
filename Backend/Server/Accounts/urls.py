a48108bfc9524be4bfa9c47d8ea735416c41aefc
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-05 11:41:49 +0330

    The Urls debugged.

--- Backend/Server/Accounts/urls.py
+++ Backend/Server/Accounts/urls.py
@@ -6,6 +6,6 @@ app_name = "Accounts"
 # Define the URL patterns for the API
 urlpatterns = [
     # URL pattern for generating a new OTP
-    path('generate-otp/', views.GenerateOTPView.as_view(), name='generate-otp'),
+    path('generate-otp/', views.AccountVerificationGenerateOTPView.as_view(), name='generate-otp'),
     path('account-verification/', views.AccountVerificationView.as_view(), name='account-verification'),
 ]
