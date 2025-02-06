ecb5dbfea8c7f344a88a1f9d8552b03a7eae8002
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-06 12:55:33 +0330

    UserTasksViewSet added to the Tasks views file.

--- Backend/Server/Tasks/views.py
+++ Backend/Server/Tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from rest_framework import status
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from .models import Tasks
+from .serializers import TasksSerializer
+
+
+
+
+class UserTasksViewSet(ViewSet):
+    def list(self, request):
+        queryset = Tasks.objects.all()
+        instance = queryset.filter(user=request.user)
+        serializer = TasksSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrive(self, request, slug):
+        queryset = Tasks.objects.all()
+        instance = queryset.filter(user=request.user, slug=slug)
+        serializer = TasksSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
