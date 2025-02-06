ee284d30f1eb105e91d78641f23298736c17105f
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-06 11:36:59 +0330

    Tasks model is added and created.

--- Backend/Server/Tasks/models.py
+++ Backend/Server/Tasks/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from Users.models import User
 
-# Create your models here.
+
+
+class Tasks(models.Model):
+
+    class TaskStatus(models.TextChoices):
+        PENDING = 'PEN'
+        COMPLETED = 'COM'
+        EXPIRED = 'EXP'
+
+
+    status = models.CharField(
+        max_length=3, 
+        choices=TaskStatus.choices, 
+        default=TaskStatus.PENDING
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='User_Tasks'
+    )
+
+    title = models.CharField(max_length=255)
+
+    description = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    dead_line = models.DateTimeField()
+
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+    
+
+    def __str__(self):
+        return self.title
